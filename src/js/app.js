7ddc7370be81be5f8d29fbce805a6a8387ac9cdd
daikon4651 <>
2024-06-18 08:18:55 +0900

    更新

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,28 @@
+// jQuery;
+// import $ from 'jquery';
+
+// $('.hero').css({
+//   color: 'tomato',
+//   backgroundColor: 'gold',
+//   'font-size': 36
+// });
+
+
+
+
+// 共通の自作JavaScript
+import './modules/common.js';
+
+
+
+
+// ES Modules
+// import greet from './modules/common.js';
+
+// greet('北');
+
+
+
+
 // Sass
 import '../scss/style.scss';
---
+++ src/js/modules/common.js
@@ -0,0 +1,14 @@
+// 共通の自作JavaScript
+
+
+// jQuery
+// import $ from 'jquery';
+
+// $('.test18').css({ color: 'aqua' });
+
+
+
+
+export default function greet(name) {
+  console.log(`おはよう。${name}さん。`);
+};
